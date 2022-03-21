49e0850eb5cc7dc5288673e07b81dca24021575d
Leonardo Henrique de Camargo <leonardohcamargo10@gmail.com>
2022-03-20 22:52:58 -0300

    enviando primeiros arquivos do projeto para o repositorio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Login from "./components/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      logado: false,
+      mensagem: ""
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(e.target.username.value);
+
+    if (!e.target.username.value) {
+      alert("Obrigatório preenchimento do usuário");
+      this.setState({ logado: false })
+    } else if (!e.target.username.value) {
+      alert("Obrigatório preenchimento do usuário válido");
+      this.setState({ logado: false })
+    } else if (!e.target.password.value) {
+      this.setState({ logado: false })
+      alert("Obrigatório preenchimento de senha");
+    } else if (
+      e.target.username.value === "eduardo.lino@pucpr.br" &&
+      e.target.password.value === "123456"
+    ) {
+      this.setState({ logado: true })
+      this.setState({ mensagem: "Acessado com sucesso!" })
+    } else {
+      this.setState({ logado: true })
+      this.setState({ mensagem: "Usuário ou senha incorretos!" })
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Login</h1>
+        <form className="form" onSubmit={this.handleSubmit}>
+          <div className="input-group">
+            <input type="username" name="username" placeholder="Usuário" />
+          </div>
+          <div className="input-group">
+            <input type="password" name="password" placeholder="Senha"/>
+          </div>
+          <button className="primary">Acessar</button>
+          {this.state.logado ? <Login mensagem={this.state.mensagem}/> : <div></div>}
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
